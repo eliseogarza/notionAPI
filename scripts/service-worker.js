2551e765121aaab27bb9dd8842633e89d6a944d7
Eliseo Garza <eliseogarza@tamu.edu>
2025-03-29 13:38:18 -0500

    Created service-worker script and adjusted html name

---
+++ scripts/service-worker.js
@@ -0,0 +1,10 @@
+console.log("service-worker")
+
+// TODO: Add this to manifest.json to change the image displayed for our extension
+// ,
+//     "icons": {
+//         "16": "images/",
+//         "32": "images/",
+//         "48": "images/",
+//         "128": "images/"
+//     }
